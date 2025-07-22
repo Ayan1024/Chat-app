4f355ff9555efa30500cd23ec50675fd7d3af647
Ayan Mukherjee <116819600+Ayan1024@users.noreply.github.com>
2025-07-23 02:09:50 +0530

    completed except socket.io part

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
-
+import cors from "cors"
 
 dotenv.config()
 const app = express();
@@ -21,8 +21,13 @@ app.use(express.urlencoded({ extended: true })); // Optional: for parsing form d
 
 app.use(cookieParser());
 
+app.use(cors({
+origin: "http://localhost:5173",
+credentials: true,
+}))
+
 app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/messages", messageRoutes)
 
 connectDB().then(() => {
   app.listen(PORT, () => {
